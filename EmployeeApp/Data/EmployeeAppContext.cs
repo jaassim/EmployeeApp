0c1157372c89e036f377e3e9e7df15e39c22ee65
jaassim <jassimjaaz32@gmail.com>
2024-06-22 18:44:21 +0530

    Add project files.

---
+++ EmployeeApp/Data/EmployeeAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeApp.Model;
+
+namespace EmployeeApp.Data
+{
+    public class EmployeeAppContext : DbContext
+    {
+        public EmployeeAppContext (DbContextOptions<EmployeeAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeApp.Model.Employee> Employee { get; set; } = default!;
+    }
+}
